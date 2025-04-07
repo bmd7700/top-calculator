66a5e8b47b64082f89697e7c6b53edd2e62cd4ce
bmd7700 <bendrt@gmail.com>
2025-04-07 16:30:19 -0400

    rough html for calculator body

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # top-calculator
 Calculator project from TOP:
 https://www.theodinproject.com/lessons/foundations-calculator
+
+Calculator should:
+    > Take input from the user in the form of two numbers and one operator
+    > Complete and display the calculation on the calculator display
+    > Have a working 'clear' function that purges all variables
+    > Calculator should only evaluate a single pair of numbers. 
+        >> If further operators are added, the calculator should complete the operation before moving to the next.
+        >> e.g. user enters "12 + 7 - 5" the calculator should compute 12 + 7, result it, and use the result for the next operation.
+        >> Round decimal answers.
+        >> When results are displayed, pressing a new digit should clear the result and start a new calculation. 
+    > Add a backspace button.
+    > Ensure only one decimal point can be entered at a time. 
--- calculator.js
+++ calculator.js
@@ -1,26 +1,11 @@
-//step 1: make functions for add, subtract, multiply, divide
-function add(num1, num2){
-    return num1 + num2;
-}
-
-function subtract(num1, num2){
-    return num1 - num2;
-}
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-function divide(num1, num2){
-    return num1 / num2;
-}
-
-//step 2: calculator operations consist of number, operator, and second number. 
-// Create three variables, one for each part of the operation. 
+// Three variables for user input: 
 let input1;
 let input2;
 let desiredOperation;
 
+// Harvest input from the user: will need event listener to capture button input. 
+
+// Call the Operate function with the user-inputted data. 
 //step 3: create a new function 'operate' that takes the operator and two numbers and calls one of the above functions.
 // need to debug this so operator can be entered without quotes
 // maybe ingest the entire expression and then parse it by symbols?
@@ -43,3 +28,17 @@ function operateTest(exp){
 //         return divide(num1, num2);
 //     } else {return 'Operator Error'}
 // }
+
+//Mathematical Operations: 
+function add(num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
