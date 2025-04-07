53ac7d6463735131ffa111a9f9ae4bd7d91d00a9
bmd7700 <bendrt@gmail.com>
2025-04-07 15:17:15 -0400

    rename input variables. resize and realign calculator numpad.

--- calculator.js
+++ calculator.js
@@ -17,22 +17,29 @@ function divide(num1, num2){
 
 //step 2: calculator operations consist of number, operator, and second number. 
 // Create three variables, one for each part of the operation. 
-let displayNum1;
-let displayNum2;
-let displayOperator;
+let input1;
+let input2;
+let desiredOperation;
 
 //step 3: create a new function 'operate' that takes the operator and two numbers and calls one of the above functions.
 // need to debug this so operator can be entered without quotes
-function operate(num1, operator, num2){
-    let op = operator;
-    console.log(op);
-    if(op == '+'){
-        return add(num1, num2);
-    } else if(op == '-'){
-        return subtract(num1, num2);
-    } else if(op == '*'){
-        return multiply(num1, num2);
-    } else if(op == '/'){
-        return divide(num1, num2);
-    } else {return 'Operator Error'}
+// maybe ingest the entire expression and then parse it by symbols?
+// been approaching this all wrong. Take input from the buttons and use that to call the functions.
+function operateTest(exp){
+    let operation = exp.toString();
+    console.log('Operation:' + operation + ' Type: ' + (typeof operation));
+
 }
+// function operate(num1, operator, num2){
+//     let op = operator;
+//     console.log(op);
+//     if(op == '+'){
+//         return add(num1, num2);
+//     } else if(op == '-'){
+//         return subtract(num1, num2);
+//     } else if(op == '*'){
+//         return multiply(num1, num2);
+//     } else if(op == '/'){
+//         return divide(num1, num2);
+//     } else {return 'Operator Error'}
+// }
