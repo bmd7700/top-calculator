2dd6c18d1b9ef7a87f70d00bf0102e21f74c55f3
bmd7700 <bendrt@gmail.com>
2025-04-10 14:20:12 -0400

    build basic function to store inputs

--- calculator.js
+++ calculator.js
@@ -1,13 +1,9 @@
-// Three variables for user input: 
-let input1;
-let input2;
-let desiredOperation;
-
 // Harvest input from the user: will need event listener to capture button input. 
 //event listener
 let buttons = document.getElementsByClassName('button');
 let buttonArray = Array.from(buttons);
 buttonArray.forEach(button =>(button.addEventListener("click", () => console.log(button.id))));
+buttonArray.forEach(button =>(button.addEventListener("click", () => operate(button.id))));
 
 //Upon clicking, the inputs should go to an array.
 //We need to collect the first number, which could be any length; an operator; and a second n-length number
@@ -17,9 +13,28 @@ buttonArray.forEach(button =>(button.addEventListener("click", () => console.log
 //  Whenever the second number is collected, call the calculation function (F1(V1,V2)) and store the result in variable (V1). 
 // Now loop back to listen for an operator -- listen for a second number -- add it to the first number, then store it in the first number...etc etc
 /* 
-
+V1, Operator, and V2 need to be displayed in the div id'd as 'display', then the answer needs to be populated there. 
 */
+// Variables for user input: 
+let input1 = '';
+let input2 = '';
+let desiredOperation = '';
+let operatorArray = ['+', '-', '*', '/'];
 
+function operate(operand){
+    if(!operatorArray.includes(operand)){ //if input is not an operator;
+        if(desiredOperation.length === 0){ //if there is no operator, store value in input1 (v1)
+            input1 = input1.concat(operand);
+            console.log('input1: ' + input1);
+        } else { //if there is an operator, store in V2 as string
+            input2 = input2.concat(operand);
+            console.log('input2: ' + input2);
+        } 
+    } else {
+        desiredOperation = operand.toString(); //stores operator as string
+        console.log('desiredOperation: ' + desiredOperation);
+    }
+}
 
 
 
