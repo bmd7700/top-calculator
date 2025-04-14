154cd4869ddcbac5f7e59df9535b8bc6584f6c02
bmd7700 <bendrt@gmail.com>
2025-04-14 13:23:07 -0400

    fix display bug: operators now display

--- calculator.js
+++ calculator.js
@@ -1,21 +1,8 @@
-// Harvest input from the user: will need event listener to capture button input. 
-//event listener
 let buttons = document.getElementsByClassName('button');
 let buttonArray = Array.from(buttons);
 buttonArray.forEach(button =>(button.addEventListener("click", () => console.log(button.id))));
 buttonArray.forEach(button =>(button.addEventListener("click", () => operate(button.id))));
 
-//Upon clicking, the inputs should go to an array.
-//We need to collect the first number, which could be any length; an operator; and a second n-length number
-//Have the script monitor inputs -- 
-// if it's a number, store it in a string (V1). 
-// If it's an operator, convert the string to a number and store it in a variable, then collect the second number (V2)
-//  Whenever the second number is collected, call the calculation function (F1(V1,V2)) and store the result in variable (V1). 
-// Now loop back to listen for an operator -- listen for a second number -- add it to the first number, then store it in the first number...etc etc
-/* 
-V1, Operator, and V2 need to be displayed in the div id'd as 'display', then the answer needs to be populated there. 
-*/
-// Variables for user input: 
 let input1 = '';
 let input2 = '';
 let desiredOperation = '';
@@ -23,17 +10,20 @@ let operatorArray = ['+', '-', '*', '/', '='];
 let memory = false;
 
 function operate(operand){
-    if(!operatorArray.includes(operand) && memory === false){ //if input is not an operator;
+    if(!operatorArray.includes(operand) && memory === false){ //if input is not an operator and memory is false;
         if(desiredOperation.length === 0){ //if there is no operator, store value in input1 (v1)
             input1 = input1.concat(operand);
+            displayResult(input1);
             console.log('input1: ' + input1);
         } else { //if there is an operator, store in V2 as string
             input2 = input2.concat(operand);
+            displayResult(input2);
             console.log('input2: ' + input2);
         } 
     } else {
         if(desiredOperation.length === 0){
             desiredOperation = operand.toString(); //stores operator as string
+            displayResult(operand);
             console.log('desiredOperation: ' + desiredOperation);
         } else {
             let num1 = parseInt(input1);
@@ -44,28 +34,33 @@ function operate(operand){
             switch(desiredOperation) {
                 case '+':
                     result = add(num1, num2);
+                    desiredOperation = '';
+                    console.log(desiredOperation);
                     break;
                 case '-':
                     result = subtract(num1, num2);
+                    desiredOperation = '';
+                    console.log(desiredOperation);
                     break;
                 case '*':
                     result = multiply(num1, num2);
+                    desiredOperation = '';
+                    console.log(desiredOperation);
                     break;
                 case '/':
                     result = divide(num1, num2);
+                    desiredOperation = '';
+                    console.log(desiredOperation);
                     break;
+                //case '=':
                 default:
                     console.log('Error: you are in the default selection in the switch.');
             }
             console.log('result: ' + result);
             input1 = result;
-            desiredOperation = '';
             input2 = '';
             memory = true;
             displayResult(result);
-            // from here, call the relevant math function, 
-            // then store the result in input1, reset input2 to empty, 
-            // reset desireOperation to empty
             // will also need a CLEAR button that clears memory (memory = false)
             //also need to display the operators in the display as it goes
         }
