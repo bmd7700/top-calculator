7a4087062ec949c10288c6078a50d7d45dd15a03
bmd7700 <bendrt@gmail.com>
2025-05-15 19:37:37 -0400

    fix display and calculator functionality

--- calculator.js
+++ calculator.js
@@ -6,6 +6,7 @@ buttonArray.forEach(button =>(button.addEventListener("click", () => operate(but
 const operatorArray = ['+', '-', '*', '/', '='];
 const numArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0','.'];
 const clear = ['clr']
+const equal = ['=']
 var input1 = [];
 var input2 = [];
 var operation = [];
@@ -22,27 +23,29 @@ function operate(input){
         if(length){
             let opp = operation.toString();
             console.log('opp: ' + opp + ' ' + typeof opp);
-            doMath(input1, input2, opp);
+            display(doMath(input1, input2, opp));
             operation.pop();
-            operation.push(input);
+            if(!equal){operation.push(input);}
             console.log('operation array should be cleared and replaced: ' + operation);
         } else {
             operation.push(input);
+            display(input);
             console.log('operation array: ' + operation);
             storeNum1 = true;
         }
-        display(input);
     }
 
     if(number){
         if(storeNum1){
             input2.push(input);
+            display(makeNumbers(input2));
             console.log('num2: ' + input2);
         } else {
             input1.push(input);
+            display(makeNumbers(input1));
             console.log('num1: ' + input1);
         }
-        display(input);
+        //display(input);
     }
 
     if(clr){
@@ -50,6 +53,7 @@ function operate(input){
     input2 = [];
     operation = [];
     storeNum1 = false;
+    display(' ');
     console.log('NUKING CALCULATOR. input1: ' + input1 + ' input2: ' + input2 + ' operation: ' + operation + ' storeNum1: ' + storeNum1);
     }
 }
@@ -73,6 +77,10 @@ function doMath(firstNum, secondNum, operator){
             break;  
     }
     console.log('result: ' + result);
+    input1 = [];
+    input1.push(result);
+    console.log('new input1: ' + input1);
+    input2 = [];
     display(result);
     return result;
 }
@@ -87,7 +95,7 @@ function makeNumbers(arr){
 function display(someNum){
     let content = '';
     const display = document.getElementById('display');
-    console.log('someNum type: ' + typeof someNum);
+    console.log('someNum type: ' + typeof someNum + ' ' + someNum);
     content = someNum.toString();
     display.innerText = content;
     }
